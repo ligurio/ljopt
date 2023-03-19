06a081b1f9bb9ccc99f5b07e5da6f7a4088776f1
Sergey Bronnikov <estetus@gmail.com>
2023-03-19 11:25:32 +0300

    initial version ljir2smt

---
+++ ljir2smt.lua
@@ -0,0 +1,12 @@
+local lj_dump = require("luajit_dump")
+local lj_bc = require("luajit_bc")
+
+local function write()
+end
+
+local function new()
+    local self = {
+        dump = {},
+    }
+    return setmetatable(t,)
+end
