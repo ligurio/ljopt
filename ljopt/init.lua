807b6fdbac6d3f0b52850218256354d29d4cda7f
Sergey Bronnikov <estetus@gmail.com>
2023-10-26 15:06:50 +0300

    Update

--- ljopt/init.lua
+++ ljopt/init.lua
@@ -1,7 +1,12 @@
 local dump_bc = require("ljopt.dump_bc")
 local dump_ir = require("ljopt.dump_ir")
 
+local parse_bc = require("ljopt.parse_bc")
+local parse_ir = require("ljopt.parse_ir")
+
 return {
     translate_ir = dump_ir,
     translate_bc = dump_bc,
+
+   _VERSION = "0.0.1"
 }
---
+++ ljopt/main.lua
@@ -0,0 +1,103 @@
+-- TODO
+-- - bisect lj opt
+-- - bisect lj opt level
+
+--tarantool> jit.version
+-----
+--- LuaJIT 2.1.0-beta3
+--...
+
+--tarantool> jit.status()
+-----
+--- true
+--- SSE2
+--- SSE3
+--- SSE4.1
+--- BMI2
+--- fold
+--- cse
+--- dce
+--- fwd
+--- dse
+--- narrow
+--- loop
+--- abc
+--- sink
+--- fuse
+--...
+
+
+local exit_codes = {
+  ok = 0,
+  warnings = 1,
+  errors = 2,
+  fatals = 3,
+  critical = 4
+}
+
+if jit == nil then
+  io.stderr:write("Unsupported Lua runtime.\n")
+  os.exit(exit_codes.critical)
+end
+
+if jit.version_num ~= 20100 then
+  io.stderr:write("LuaJIT version mismatch.\n")
+  os.exit(exit_codes.critical)
+end
+
+local lua_code = arg[1]
+if lua_code == nil or
+   lua_code == "-" then
+  lua_code = io.stdin:read("*a") -- read the complete stdin
+end
+
+-- print(lua_code)
+
+-- Lua treats any independent chunk as the body of an anonymous function. For
+-- instance, for the chunk "a = 1", loadstring returns the equivalent of
+-- `function () a = 1 end`.
+-- https://www.lua.org/pil/8.html
+
+local fn, err = loadstring(lua_code)
+if fn == nil then
+  io.stderr:write(("cannot load Lua code: %s.\n"):format(err))
+  os.exit(exit_codes.fatals)
+end
+
+local function bc_dump(f)
+  -- https://luajit.org/extensions.html#string_dump
+  local bc = string.dump(f)
+  assert(bc, "bytecode is nil")
+end
+
+local function ir_dump(f)
+  -- TODO
+end
+
+local function bc_parse(f)
+  -- TODO
+end
+
+local function ir_parse(f)
+  -- TODO
+end
+
+-- https://luajit.org/running.html
+local default_states = {
+  "jit.opt.start(0, 'hotloop=1', 'hotexit=1')",
+  "jit.opt.start(3, 'hotloop=1', 'hotexit=1')",
+}
+
+local states = default_states
+local ir_res = {}
+local bc_res = {}
+for i, state in ipairs(states) do
+  io.stdout:write(state .. "\n")
+  -- TODO: print current LuaJIT state.
+  local raw_bc = bc_dump(fn)
+  bc_res[i] = bc_parse(raw_bc)
+  local raw_ir = ir_dump(fn)
+  ir_res[i] = ir_parse(raw_ir)
+end
+
+-- TODO: compare results in ir_res and bc_res.
