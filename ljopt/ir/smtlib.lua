76f6f0b825cf55ed7a34e9d204e6d35344379991
Sergey Bronnikov <estetus@gmail.com>
2023-11-01 17:09:56 +0300

    update tables with unsupported instructions and types

--- ljopt/ir/smtlib.lua
+++ ljopt/ir/smtlib.lua
@@ -3,163 +3,153 @@
 
 -- IR Types blacklist
 local ir_types_bl = {
-    -- nil
-	-- fal
-	-- tru
-	-- lud
-	-- str
-	-- p32
-	-- thr
-	-- pro
-	-- fun
-	-- p64
-	-- cdt
-	-- tab
-	-- udt
-	-- flt
-	-- num
-	-- i8
-	-- u8
-	-- i16
-	-- u16
-	-- int
-	-- u32
-	-- i64
-	-- u64
-	-- sfp
+    ["nil"] = true,
+    ["fal"] = true,
+    ["tru"] = true,
+    ["lud"] = true,
+    ["str"] = true,
+    ["p32"] = true,
+    ["thr"] = true,
+    ["pro"] = true,
+    ["fun"] = true,
+    ["p64"] = true,
+    ["cdt"] = true,
+    ["tab"] = true,
+    ["udt"] = true,
+    ["flt"] = true,
+    ["num"] = true,
+    ["i8"] = true,
+    ["u8"] = true,
+    ["i16"] = true,
+    ["u16"] = true,
+    ["int"] = true,
+    ["u32"] = true,
+    ["i64"] = true,
+    ["u64"] = true,
+    ["sfp"] = true,
 }
 
 -- IR instructions blacklist
 
 local ir_ins_bl = {
-    -- Constants
-	-- KPRI
-	-- KINT
-	-- KGC
-	-- KPTR
-	-- KKPTR
-	-- KNULL
-	-- KNUM
-	-- KINT64
-	-- KSLOT
-
-	-- Guarded Assertions
-	-- OP
-	-- LT
-	-- GE
-	-- LE
-	-- GT
-	-- ULT
-	-- UGE
-	-- ULE
-	-- UGT
-	-- EQ
-	-- NE
-	-- ABC
-	-- RETF
-
-	-- Bit Ops
-	-- BNOT
-	-- BSWAP
-	-- BAND
-	-- BOR
-	-- BXOR
-	-- BSHL
-	-- BSHR
-	-- BSAR
-	-- BROL
-	-- BROR
-
-	-- Arithmetic Ops
-	-- ADD
-	-- SUB
-	-- MUL
-	-- DIV
-	-- MOD
-	-- POW
-	-- NEG
-	-- ABS
-	-- ATAN2
-	-- LDEXP
-	-- MIN
-	-- MAX
-	-- FPMATH
-	-- ADDOV
-	-- SUBOV
-	-- MULOV
-	-- FPM_FLOOR
-	-- FPM_CEIL
-	-- FPM_TRUNC
-	-- FPM_SQRT
-	-- FPM_EXP
-	-- FPM_EXP2
-	-- FPM_LOG
-	-- FPM_LOG2
-	-- FPM_LOG10
-	-- FPM_SIN
-	-- FPM_COS
-	-- FPM_TAN
-
-	-- Memory References
-	-- AREF
-	-- HREFK
-	-- HREF
-	-- NEWREF
-	-- UREFO
-	-- UREFC
-	-- FREF
-	-- STRREF
-
-	-- Loads and Stores
-	-- ALOAD
-	-- HLOAD
-	-- ULOAD
-	-- FLOAD
-	-- XLOAD
-	-- SLOAD
-	-- VLOAD
-	-- ASTORE
-	-- HSTORE
-	-- USTORE
-	-- FSTORE
-	-- XSTORE
-
-	-- Allocations
-	-- SNEW
-	-- XSNEW
-	-- TNEW
-	-- TDUP
-	-- CNEW
-	-- CNEWI
-
-	-- Barriers
-	-- TBAR
-	-- OBAR
-	-- XBAR
-
-	-- Type Conversions
-	-- CONV
-	-- TOBIT
-	-- TOSTR
-	-- STRTO
-
-	-- Calls
-	-- CALLN
-	-- CALLL
-	-- CALLS
-	-- CALLXS
-	-- CARG
-
-	-- Miscellaneous Ops
-	-- NOP
-	-- BASE
-	-- PVAL
-	-- GCSTEP
-	-- HIOP
-	-- LOOP
-	-- USE
-	-- PHI
-	-- RENAME
+    -- Constants.
+    ["KPRI"] = true,
+    ["KINT"] = true,
+    ["KGC"] = true,
+    ["KPTR"] = true,
+    ["KKPTR"] = true,
+    ["KNULL"] = true,
+    ["KNUM"] = true,
+    ["KINT64"] = true,
+    ["KSLOT"] = true,
+    -- Guarded Assertions.
+    ["OP"] = true,
+    ["LT"] = true,
+    ["GE"] = true,
+    ["LE"] = true,
+    ["GT"] = true,
+    ["ULT"] = true,
+    ["UGE"] = true,
+    ["ULE"] = true,
+    ["UGT"] = true,
+    ["EQ"] = true,
+    ["NE"] = true,
+    ["ABC"] = true,
+    ["RETF"] = true,
+    -- Bit Ops.
+    ["BNOT"] = true,
+    ["BSWAP"] = true,
+    ["BAND"] = true,
+    ["BOR"] = true,
+    ["BXOR"] = true,
+    ["BSHL"] = true,
+    ["BSHR"] = true,
+    ["BSAR"] = true,
+    ["BROL"] = true,
+    ["BROR"] = true,
+    -- Arithmetic Ops.
+    ["ADD"] = true,
+    ["SUB"] = true,
+    ["MUL"] = true,
+    ["DIV"] = true,
+    ["MOD"] = true,
+    ["POW"] = true,
+    ["NEG"] = true,
+    ["ABS"] = true,
+    ["ATAN2"] = true,
+    ["LDEXP"] = true,
+    ["MIN"] = true,
+    ["MAX"] = true,
+    ["FPMATH"] = true,
+    ["ADDOV"] = true,
+    ["SUBOV"] = true,
+    ["MULOV"] = true,
+    ["FPM_FLOOR"] = true,
+    ["FPM_CEIL"] = true,
+    ["FPM_TRUNC"] = true,
+    ["FPM_SQRT"] = true,
+    ["FPM_EXP"] = true,
+    ["FPM_EXP2"] = true,
+    ["FPM_LOG"] = true,
+    ["FPM_LOG2"] = true,
+    ["FPM_LOG10"] = true,
+    ["FPM_SIN"] = true,
+    ["FPM_COS"] = true,
+    ["FPM_TAN"] = true,
+    -- Memory References.
+    ["AREF"] = true,
+    ["HREFK"] = true,
+    ["HREF"] = true,
+    ["NEWREF"] = true,
+    ["UREFO"] = true,
+    ["UREFC"] = true,
+    ["FREF"] = true,
+    ["STRREF"] = true,
+    -- Loads and Stores.
+    ["ALOAD"] = true,
+    ["HLOAD"] = true,
+    ["ULOAD"] = true,
+    ["FLOAD"] = true,
+    ["XLOAD"] = true,
+    ["SLOAD"] = true,
+    ["VLOAD"] = true,
+    ["ASTORE"] = true,
+    ["HSTORE"] = true,
+    ["USTORE"] = true,
+    ["FSTORE"] = true,
+    ["XSTORE"] = true,
+    -- Allocations.
+    ["SNEW"] = true,
+    ["XSNEW"] = true,
+    ["TNEW"] = true,
+    ["TDUP"] = true,
+    ["CNEW"] = true,
+    ["CNEWI"] = true,
+    -- Barriers.
+    ["TBAR"] = true,
+    ["OBAR"] = true,
+    ["XBAR"] = true,
+    -- Type Conversions.
+    ["CONV"] = true,
+    ["TOBIT"] = true,
+    ["TOSTR"] = true,
+    ["STRTO"] = true,
+    -- Calls.
+    ["CALLN"] = true,
+    ["CALLL"] = true,
+    ["CALLS"] = true,
+    ["CALLXS"] = true,
+    ["CARG"] = true,
+    -- Miscellaneous Ops.
+    ["NOP"] = true,
+    ["BASE"] = true,
+    ["PVAL"] = true,
+    ["GCSTEP"] = true,
+    ["HIOP"] = true,
+    ["LOOP"] = true,
+    ["USE"] = true,
+    ["PHI"] = true,
+    ["RENAME"] = true,
 }
 
 local function is_supported_ir_ins(ins)
