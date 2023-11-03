352a222ecdcb85043a712f36d0204e245aa21f4b
Sergey Bronnikov <estetus@gmail.com>
2023-11-03 11:00:44 +0300

    bc_smtlib: convert comment to table with blacklist

--- ljopt/bc_smtlib.lua
+++ ljopt/bc_smtlib.lua
@@ -1,132 +1,114 @@
 -- BC to SMT-LIB
 -- https://github.com/tarantool/tarantool/wiki/LuaJIT-Bytecodes
 
--- Comparison ops
-
--- ISLT
--- ISGE
--- ISLE
--- ISGT
--- ISEQV
--- ISNEV
--- ISEQS
--- ISNES
--- ISEQN
--- ISNEN
--- ISEQP
--- ISNEP
-
--- Unary Test and Copy ops
-
--- ISTC
--- ISFC
--- IST
--- ISF
-
--- Unary ops
-
--- MOV
--- NOT
--- UNM
--- LEN
-
--- Binary ops
-
--- ADDVN
--- SUBVN
--- MULVN
--- DIVVN
--- MODVN
--- ADDNV
--- SUBNV
--- MULNV
--- DIVNV
--- MODNV
--- ADDVV
--- SUBVV
--- MULVV
--- DIVVV
--- MODVV
--- POW
--- CAT
-
--- Constant ops
-
--- KSTR
--- KCDATA
--- KSHORT
--- KNUM
--- KPRI
--- KNIL
-
--- Upvalue and Function ops
-
--- UGET
--- USETV
--- USETS
--- USETN
--- USETP
--- UCLO
--- FNEW
-
--- Table ops
-
--- TNEW
--- TDUP
--- GGET
--- GSET
--- TGETV
--- TGETS
--- TGETB
--- TSETV
--- TSETS
--- TSETB
--- TSETM
-
--- Calls and Vararg Handling
-
--- CALLM
--- CALL
--- CALLMT
--- CALLT
--- ITERC
--- ITERN
--- VARG
--- ISNEXT
-
--- Returns
-
--- RETM
--- RET
--- RET0
--- RET1
-
--- Loops and branches
-
--- FORI
--- JFORI
--- FORL
--- IFORL
--- JFORL
--- ITERL
--- IITERL
--- JITERL
--- LOOP
--- ILOOP
--- JLOOP
--- JMP
-
--- Function headers
-
--- FUNCF
--- IFUNCF
--- JFUNCF
--- FUNCV
--- IFUNCV
--- JFUNCV
--- FUNCC
--- FUNCCW
--- FUNC*
+-- BC ops blacklist.
+local bc_ops_bl = {
+    -- Comparison ops
+    ["ISLT"] = false,
+    ["ISGE"] = false,
+    ["ISLE"] = false,
+    ["ISGT"] = false,
+    ["ISEQV"] = false,
+    ["ISNEV"] = false,
+    ["ISEQS"] = false,
+    ["ISNES"] = false,
+    ["ISEQN"] = false,
+    ["ISNEN"] = false,
+    ["ISEQP"] = false,
+    ["ISNEP"] = false,
+    -- Unary Test and Copy ops
+    ["ISTC"] = false,
+    ["ISFC"] = false,
+    ["IST"] = false,
+    ["ISF"] = false,
+    -- Unary ops
+    ["MOV"] = false,
+    ["NOT"] = false,
+    ["UNM"] = false,
+    ["LEN"] = false,
+    -- Binary ops
+    ["ADDVN"] = false,
+    ["SUBVN"] = false,
+    ["MULVN"] = false,
+    ["DIVVN"] = false,
+    ["MODVN"] = false,
+    ["ADDNV"] = false,
+    ["SUBNV"] = false,
+    ["MULNV"] = false,
+    ["DIVNV"] = false,
+    ["MODNV"] = false,
+    ["ADDVV"] = false,
+    ["SUBVV"] = false,
+    ["MULVV"] = false,
+    ["DIVVV"] = false,
+    ["MODVV"] = false,
+    ["POW"] = false,
+    ["CAT"] = false,
+    -- Constant ops
+    ["KSTR"] = false,
+    ["KCDATA"] = false,
+    ["KSHORT"] = false,
+    ["KNUM"] = false,
+    ["KPRI"] = false,
+    ["KNIL"] = false,
+    -- Upvalue and Function ops
+    ["UGET"] = false,
+    ["USETV"] = false,
+    ["USETS"] = false,
+    ["USETN"] = false,
+    ["USETP"] = false,
+    ["UCLO"] = false,
+    ["FNEW"] = false,
+    -- Table ops
+    ["TNEW"] = false,
+    ["TDUP"] = false,
+    ["GGET"] = false,
+    ["GSET"] = false,
+    ["TGETV"] = false,
+    ["TGETS"] = false,
+    ["TGETB"] = false,
+    ["TSETV"] = false,
+    ["TSETS"] = false,
+    ["TSETB"] = false,
+    ["TSETM"] = false,
+    -- Calls and Vararg Handling
+    ["CALLM"] = false,
+    ["CALL"] = false,
+    ["CALLMT"] = false,
+    ["CALLT"] = false,
+    ["ITERC"] = false,
+    ["ITERN"] = false,
+    ["VARG"] = false,
+    ["ISNEXT"] = false,
+    -- Returns
+    ["RETM"] = false,
+    ["RET"] = false,
+    ["RET0"] = false,
+    ["RET1"] = false,
+    -- Loops and branches
+    ["FORI"] = false,
+    ["JFORI"] = false,
+    ["FORL"] = false,
+    ["IFORL"] = false,
+    ["JFORL"] = false,
+    ["ITERL"] = false,
+    ["IITERL"] = false,
+    ["JITERL"] = false,
+    ["LOOP"] = false,
+    ["ILOOP"] = false,
+    ["JLOOP"] = false,
+    ["JMP"] = false,
+    -- Function headers
+    ["FUNCF"] = false,
+    ["IFUNCF"] = false,
+    ["JFUNCF"] = false,
+    ["FUNCV"] = false,
+    ["IFUNCV"] = false,
+    ["JFUNCV"] = false,
+    ["FUNCC"] = false,
+    ["FUNCCW"] = false,
+    ["FUNC*"] = false,
+}
 
 local function translate()
 end
