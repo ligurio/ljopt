29eecf06fe49512408bbcbb79bd9289b58f4598f
litnareso <litnareso@protonmail.com>
2023-12-06 18:51:50 +0300

    fix path

--- ljopt/init.lua
+++ ljopt/init.lua
@@ -1,7 +1,7 @@
 local dump_ir = require("ljopt.ir_dump")
 local dump_bc = require("ljopt.bc_dump")
 local smtlib_bc = require("ljopt.bc_smtlib")
-local smtlib_ir = require("ljopt.ir_smtlib")
+local smtlib_ir = require("ljopt.ir_smtlib.ir_smtlib")
 local jit = require("jit")
 
 if (not jit or jit.version_num ~= 20100) then
