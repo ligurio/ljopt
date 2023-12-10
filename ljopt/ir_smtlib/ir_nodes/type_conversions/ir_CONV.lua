b6fc79f6c63900b2101b727d8f516f3c899279c6
Artur Sibgatullin <artur.sibgatullin@rfdyn.ru>
2023-12-10 18:58:34 +0300

    NEW: Support CONV instuctions.

--- ljopt/ir_smtlib/ir_nodes/type_conversions/ir_CONV.lua
+++ ljopt/ir_smtlib/ir_nodes/type_conversions/ir_CONV.lua
@@ -3,9 +3,22 @@ local ir_node = require("ljopt/ir_smtlib/ir_nodes/ir_node_base")
 ir_node_CONV = {}
 ir_node.extended(ir_node_CONV, ir_node.ir_node_base)
 
-function ir_node_CONV:to_smt_lib()
-    --TODO: Implement
-    return ''
+function ir_node_CONV:to_smt_lib(ctx)
+    local left_op = ''
+    local right_op = self:get_right_op()
+    local data = ''
+
+    -- TODO: Support other conversions
+    if right_op == "num.int" then
+        left_op = self:retrieve_int_op(self:get_left_op(), ctx)
+        left_op = left_op:gsub("+", "")
+
+        data = string.format("((_ to_fp 11 53) roundNearestTiesToEven %s 0.)", left_op)
+    else
+        assert(false, "Unsupported type conversion")
+    end
+
+    return ctx.op_stack:store(self:get_ssa_reference(), self:get_type(), data)
 end
 
 function instance(ssa_ref, flags, type, left_op, right_op)
