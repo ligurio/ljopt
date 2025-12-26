bc4e4e5d04b51cb49f7c4c7ac7252528582c0e18
Churkin Aleksey <lekhachurkin@gmail.com>
2025-12-26 21:21:05 +0300

    ljopt: support SIMD constants
    LuaJIT has multiple predefined fp-64 constants. However, we're
    interested only in 2 of them - LJ_KSIMD_ABS and LJ_KSIMD_NEG since
    they appear in trace dumps. Other constants used only in runtime.

--- ljopt/ir/FLOAD.lua
+++ ljopt/ir/FLOAD.lua
@@ -1,3 +1,5 @@
+local ffi = require('ffi')
+
 local ir_node = require('ljopt.ir.ir_node_base')
 
 local IRNodeFLOAD = {}
@@ -10,6 +12,36 @@ function IRNodeFLOAD:to_smt_lib(ctx)
 
     -- TODO: Support other fields.
     -- TODO: cdata.type can have different IR type?
+    if self:get_left_op() == 'nil' then
+        -- Predefined constants.
+        -- Offset may be different depending on platform.
+        -- on x86-64 it's correct.
+        -- Apparently whether they differ depends only
+        -- on value of LUAJIT_ENABLE_GC64.
+        -- Note: That's all constants we need. Other
+        -- constants appear only in asm.
+        if ffi.abi('gc64') then
+            if right_op == '#306' then
+                data = self:retrieve_num_op('#x8000000000000000', ctx)
+            elseif right_op == '#302' then
+                data = self:retrieve_num_op('#x7fffffffffffffff', ctx)
+            else
+                assert(false,
+                    'Unreachable. Other constants should not be here.'
+                )
+            end
+        else
+            if right_op == '#226' then
+                data = self:retrieve_num_op('#x8000000000000000', ctx)
+            elseif right_op == '#222' then
+                data = self:retrieve_num_op('#x7fffffffffffffff', ctx)
+            else
+                assert(false,
+                    'Unreachable. Other constants should not be here.'
+                )
+            end
+        end
+    end
     if right_op == 'cdata.int64' then
         left_op = self:retrieve_i64_op(self:get_left_op(), ctx)
         data = left_op
