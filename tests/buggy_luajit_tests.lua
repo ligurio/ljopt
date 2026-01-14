7653ca28e1c91e1d8f5a403322334b6ba884dfe6
Sergey Bronnikov <estetus@gmail.com>
2026-01-14 10:01:57 +0300

    tests: refactoring in buggy_luajit_tests
    - Export the function `cwd()` in coverage Lua module, it is
      required for setting current directory in tests.
    - Introduce two functions `reproduce_bug_in_runtime()` and
      `reproduce_bug_using_smt()` for using in tests. First one checks
      that Lua chunk reproduce a bug, it is a strict requirement before
      reproducing the bug using SMT verification. The second one
      reproduce a bug using translation traces to SMT formulas and
      checking their equivalence by SMT solver.
    - Introduce a helper `read_file()` for reading files with Lua
      chunks. Storing reproducers in a separate files is more convenient
      than using multiline string, the following commit introduce
      several dozen reproducers and many of them are huge for multiline
      string.
    
    Related to #18
    Related to #16

--- tests/buggy_luajit_tests.lua
+++ tests/buggy_luajit_tests.lua
@@ -10,47 +10,88 @@ local ljopt = require("ljopt")
 local smt = require("tests.smtlib2").new()
 local test = require("tests.tap").test("ljopt")
 local smt_constants = require('ljopt.smt_constants')
+local coverage = require("tests.coverage")
+local toggle_debug_hook = require('tests.coverage').toggle_debug_hook()
 
-local buggy_build = os.getenv("BUGGY_BUILD")
+local buggy_build = os.getenv("BUGGY_BUILD") == "1"
+local reproducers_path = coverage.cwd() .. "/tests/reproducers/"
 
 -- NOOP when environment variable LJOPT_COVERAGE is undefined.
-require("tests.coverage").enable()
+coverage.enable()
 
 test:plan(1)
 
-local function test_result_expected(test, id, formula)
-    test:is(smt:parse(formula), true, "formula_" .. id .. " parsed")
+-- The function executes the passed Lua chunk and returns
+-- a boolean value - true if the result of execution is as
+-- expected: the bug was reproduced on the problematic LuaJIT
+-- version and the bug was not reproduced on the fixed LuaJIT
+-- version and false otherwise.
+local function reproduce_bug_in_runtime(chunk, err_msg)
+    local fn = assert(load(chunk), "Lua chunk is broken")
+    -- Enabled code coverage breaks LuaJIT compilation.
+    toggle_debug_hook()
+    local ok, err = pcall(fn)
+    toggle_debug_hook()
+    local expected_result = not buggy_build
+    -- Default value is false, when LuaJIT build contains a bug.
+    local err_is_matched = not buggy_build
     if buggy_build then
-        test:is(smt:check(formula), smt.result.SAT, "test_buggy_luajit_" .. id)
-    else
-        test:is(smt:check(formula), smt.result.UNSAT, "test_correct_" .. id)
+       assert(type(err) == "string", "error is not a string")
+       err_is_matched = string.match(err, err_msg) ~= nil
     end
-
+    return ok == expected_result and err_is_matched
 end
 
+-- The function records LuaJIT traces, translate every trace to
+-- SMT-LIB formulas, check their SMT-LIB syntax and check that
+-- SMT solver returns an expected result for produced formulas.
+-- The function returns true if everything was succeed and false
+-- otherwise. The function triggers an assertion if no SMT
+-- formulas were produced.
+local function reproduce_bug_using_smt(chunk)
+    -- Enabled code coverage breaks LuaJIT compilation,
+    -- `toggle_debug_hook()` disables using `debug.sethook()` in
+    -- <ljopt/ir_dump.lua>.
+    local formulas = ljopt.ir.traces_to_smt(chunk)
+    assert(next(formulas) ~= nil, "no SMT formulas")
+    for _, formula in pairs(formulas) do
+        local smt_formula = smt_constants.LJOPT_SMTLIB .. formula
+        if not smt:parse(smt_formula) then
+            return false
+        end
+        local smt_res = buggy_build and smt.result.SAT or smt.result.UNSAT
+        if smt:check(smt_formula) ~= smt_res then
+            return false
+        end
+    end
+
+    return true
+end
+
+local function read_reproducer_file(filename)
+    local path = reproducers_path .. filename
+    local file = io.open(path, "r")
+    if not file then
+        return nil
+    end
+    local buf = file:read("*a")
+    file:close()
+    return buf
+end
+
+-- https://github.com/LuaJIT/LuaJIT/pull/783
 -- luacheck: push no max_comment_line_length
--- https://github.com/LuaJIT/LuaJIT/commit/ab0c0793a43fc0fb0c7b71b6250339117d99254a
+-- https://github.com/tarantool/luajit/commit/ab0c0793a43fc0fb0c7b71b6250339117d99254a
+-- https://github.com/LuaJIT/LuaJIT/commit/7b994e0ee0399caf6319865bbac88ddf62129a36
 -- luacheck: pop
-test:test("Test fold_signed_zero (LuaJIT#783)", function(test)
-    local min_zero = [[
-local function foo(a)
-    return a - (-0.0)
-end
-foo(-0.0)
-foo(-0.0)
-foo(-0.0)
-foo(-0.0)
-foo(-0.0)
-foo(-0.0)
-]]
+test:test("Fix FOLD rule for x-0 (LuaJIT#783)", function(test)
     test:plan(2)
-
-    local formulas = ljopt.ir.traces_to_smt(min_zero)
-    for i, formula in pairs(formulas) do
-        test_result_expected(test, i, smt_constants.LJOPT_SMTLIB .. formula)
-    end
+    local chunk = read_reproducer_file("lj_783.lua")
+    test:ok(reproduce_bug_in_runtime(chunk,
+        "-0 folding in simplify_numsub_k"), "reproduce in runtime")
+    test:ok(reproduce_bug_using_smt(chunk), "reproduce using SMT")
 end)
 
-require("tests.coverage").shutdown()
+coverage.shutdown()
 
 os.exit(test:check() == true and 0 or 1)
