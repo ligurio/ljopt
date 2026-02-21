5d6735150972d5dc25b2455c4c527396d4478a7c
Churkin Aleksey <lekhachurkin@gmail.com>
2026-02-21 15:12:14 +0300

    ljopt: add to trace id arguments types
    This will improve trace matching when we have same bytecode
    for different traces due to different types.
    
    Problem in issue 26 was that Lua could generate multiple traces
    with the same bytecode, but different types (probably it can
    generate multiple traces with same bytecode, same types, but
    different values, we haven't meet that yet). Adding types
    to trace hash makes ID more unique.
    Fixes #26

--- ljopt/smt_constants.lua
+++ ljopt/smt_constants.lua
@@ -4,9 +4,11 @@ local MAXSNAP = 500
 
 -- This variable contains SMT library helpers to be used
 -- by ljopt.
+-- luacheck: push no max_line_length
 local LJOPT_SMTLIB = ([[
-(define-fun lsb ((x (_ BitVec %d))) (_ BitVec %d) (bvand x (bvneg x)))
-]]):format(MAXSNAP, MAXSNAP)
+(define-fun lsb ((x (_ BitVec %d))) (_ BitVec %d) (ite (not (= x (_ bv0 %d))) (_ bv1 %d) (_ bv0 %d)))
+]]):format(MAXSNAP, MAXSNAP, MAXSNAP, MAXSNAP, MAXSNAP)
+-- luacheck: pop
 
 return {
     LJOPT_SMTLIB = LJOPT_SMTLIB,
