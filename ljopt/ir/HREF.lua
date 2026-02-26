811cb9b790b80c7c06c874657bd755d6954c3e8e
Churkin Aleksey <lekhachurkin@gmail.com>
2026-02-26 22:36:32 +0300

    ljopt: support hash part of table IRs
    Add support for FREF, FSTORE, HLOAD, HREF, HREFK, HSTORE, NEWREF.
    Now hash part related read/write operations over tables supported
    in ljopt. All required infrastructure was introduced in previous
    commit, here we simply use MemoryStack API.

---
+++ ljopt/ir/HREF.lua
@@ -0,0 +1,35 @@
+local arith_utils = require('ljopt.ir.arith_utils')
+local ir_node = require('ljopt.ir.ir_node_base')
+local utils = require('ljopt.utils')
+
+local impls = {}
+
+impls.IRNodeHREFP32 = {}
+ir_node.extended(impls.IRNodeHREFP32, ir_node.ir_node_base)
+
+function impls.IRNodeHREFP32:to_smt_lib(ctx)
+    local left_op = self:get_left_op()
+    local right_op = self:get_right_op()
+    local id
+    if right_op ~= nil and string.sub(right_op, 1, 1) == '"' then
+        -- Drop @0 etc
+        local s = right_op:gsub('^([^"]*"[^"]*").*', '%1')
+        id = arith_utils.const_int_to_smt_bv(tonumber(utils.hash(s)))
+    else
+        id = ir_node.retrieve_i64_op(right_op, ctx, 'i64')
+    end
+    local ssa_ref = self:get_ssa_reference()
+    ctx.tab_info[ssa_ref] = ctx.tab_info[tonumber(left_op)]
+    return ('%s\n%s'):format(
+        ctx.te_stack:store(ssa_ref, 'true'),
+        ctx.op_stack:store(ssa_ref, self:get_type(), id)
+    )
+end
+
+local function instance(node_str)
+    return impls[node_str]
+end
+
+return {
+    instance = instance
+}
---
+++ ljopt/ir/HSTORE.lua
@@ -0,0 +1,94 @@
+local arith_utils = require('ljopt.ir.arith_utils')
+local ir_node = require('ljopt.ir.ir_node_base')
+local utils = require('ljopt.utils')
+
+local impls = {}
+
+impls.IRNodeHSTOREStr = {}
+ir_node.extended(impls.IRNodeHSTOREStr, ir_node.ir_node_base)
+
+function impls.IRNodeHSTOREStr:to_smt_lib(ctx)
+    local left_op = self:get_left_op()
+    local right_op = self:get_right_op()
+
+    local dst_slot = tonumber(left_op)
+
+    -- Table pointer. Compile time.
+    local tab_left = ctx.tab_info[dst_slot].mem_ref
+    -- Table index. Runtime.
+    local idx_left = ir_node.retrieve_i64_op(left_op, ctx, 'i64')
+    idx_left = ('(bv2int %s)'):format(idx_left)
+    local value
+    if right_op ~= nil and string.sub(right_op, 1, 1) == '"' then
+        value = arith_utils.const_int_to_smt_bv(
+            tonumber(utils.hash(right_op))
+        )
+    else
+        value = ir_node.retrieve_i64_op(right_op, ctx, 'i64')
+    end
+
+    assert(tab_left ~= nil, dst_slot)
+
+    return ctx.mem_stack:store_index(tab_left, idx_left, value)
+end
+
+impls.IRNodeHSTORENum = {}
+ir_node.extended(impls.IRNodeHSTORENum, ir_node.ir_node_base)
+
+function impls.IRNodeHSTORENum:to_smt_lib(ctx)
+    local left_op = self:get_left_op()
+    local right_op = self:get_right_op()
+
+    local dst_slot = tonumber(left_op)
+
+    -- Table pointer. Compile time.
+    local tab_left = ctx.tab_info[dst_slot].mem_ref
+    -- Table index. Runtime.
+    local idx_left = ir_node.retrieve_i64_op(left_op, ctx, 'i64')
+    idx_left = ('(bv2int %s)'):format(idx_left)
+    local value
+    if right_op ~= nil and string.sub(right_op, 1, 1) == '"' then
+        value = arith_utils.const_num_to_smt_bv(tonumber(utils.hash(right_op)))
+    else
+        value = ir_node.retrieve_i64_op(right_op, ctx, 'i64')
+    end
+
+    assert(tab_left ~= nil, dst_slot)
+
+    return ctx.mem_stack:store_index(tab_left, idx_left, value)
+end
+
+impls.IRNodeHSTORETab = {}
+ir_node.extended(impls.IRNodeHSTORETab, ir_node.ir_node_base)
+
+-- 0019    tab HSTORE 0016  0004
+-- 1. Get reference at 0019.
+-- 2. Get table uid at 0004.
+-- 3. Write table_uid to ref (and add some string
+-- to avoid hash collisions with left table metadata).
+function impls.IRNodeHSTORETab:to_smt_lib(ctx)
+    -- Assume it's a stack slot, like `0001`.
+    local left_op = self:get_left_op()
+
+    local right_op = self:get_right_op()
+
+    local dst_slot = tonumber(left_op)
+    local src_slot = tonumber(right_op)
+
+    -- Table pointer. Compile time.
+    local tab_left = ctx.tab_info[dst_slot].mem_ref
+    -- Table index. Runtime.
+    local idx_right = ctx.mem_stack:key_id(src_slot)
+    idx_right = arith_utils.const_int_to_smt_bv(idx_right)
+    local idx_left = ir_node.retrieve_i64_op(left_op, ctx, 'i64')
+    idx_left = ('(bv2int %s)'):format(idx_left)
+    return ctx.mem_stack:store_index(tab_left, idx_left, idx_right)
+end
+
+local function instance(node_str)
+    return impls[node_str]
+end
+
+return {
+    instance = instance
+}
