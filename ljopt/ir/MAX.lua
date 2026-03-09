69d0259cd1c7f27f38a7b7af9fe8dd9a105633fb
Churkin Aleksey <lekhachurkin@gmail.com>
2026-03-09 14:20:17 +0300

    ljopt: remove MIN MAX for i64
    LuaJIT implementation supports MIN and MAX for the number and
    int32 types, but not for int64. Therefore, the translation
    for these types has been removed.

--- ljopt/ir/MAX.lua
+++ ljopt/ir/MAX.lua
@@ -22,22 +22,6 @@ function impls.IRNodeMAXInt:to_smt_lib(ctx)
     return ctx.op_stack:store(self:get_ssa_reference(), self:get_type(), data)
 end
 
-impls.IRNodeMAXI64 = {}
-ir_node.extended(impls.IRNodeMAXI64, bin_op.BinOpI64)
-
-function impls.IRNodeMAXI64:to_smt_lib(ctx)
-    local left_op = ir_node.retrieve_i64_op(
-        self:get_left_op(), ctx, self:get_type()
-    )
-    local right_op = ir_node.retrieve_i64_op(
-        self:get_right_op(), ctx, self:get_type()
-    )
-    local data = string.format('(ite (bvsge %s %s) %s %s))',
-        left_op, right_op, left_op, right_op
-    )
-    return ctx.op_stack:store(self:get_ssa_reference(), self:get_type(), data)
-end
-
 local function instance(node_str)
     return impls[node_str]
 end
--- ljopt/ir/MIN.lua
+++ ljopt/ir/MIN.lua
@@ -22,22 +22,6 @@ function impls.IRNodeMINInt:to_smt_lib(ctx)
     return ctx.op_stack:store(self:get_ssa_reference(), self:get_type(), data)
 end
 
-impls.IRNodeMINI64 = {}
-ir_node.extended(impls.IRNodeMINI64, bin_op.BinOpI64)
-
-function impls.IRNodeMINI64:to_smt_lib(ctx)
-    local left_op = ir_node.retrieve_i64_op(
-        self:get_left_op(), ctx, self:get_type()
-    )
-    local right_op = ir_node.retrieve_i64_op(
-        self:get_right_op(), ctx, self:get_type()
-    )
-    local data = string.format('(ite (bvsle %s %s) %s %s))',
-        left_op, right_op, left_op, right_op
-    )
-    return ctx.op_stack:store(self:get_ssa_reference(), self:get_type(), data)
-end
-
 local function instance(node_str)
     return impls[node_str]
 end
