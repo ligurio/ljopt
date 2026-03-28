cc54c832588cc62a395e0bbb5fab39443503ee8a
Churkin Aleksey <lekhachurkin@gmail.com>
2026-03-28 16:01:40 +0300

    ljopt: write guard in NOP
    Old versions of Lua sometimes mark NOP as a guarded instruction.
    If we don't write `true` to te_stack its value becomes undefined
    and final formula gives us SAT.

--- ljopt/ir/NOP.lua
+++ ljopt/ir/NOP.lua
@@ -3,8 +3,15 @@ local ir_node = require('ljopt.ir.ir_node_base')
 local IRNodeNOP = {}
 ir_node.extended(IRNodeNOP, ir_node.ir_node_base)
 
-function IRNodeNOP:to_smt_lib()
-    return ''
+function IRNodeNOP:to_smt_lib(ctx)
+    local ssa_ref = self:get_ssa_reference()
+    if self:get_flags().irt_guard then
+        -- Old versions of Lua sometimes have
+        -- NOP flag set to `true`.
+        return ctx.te_stack:store(ssa_ref, 'true') .. '\n'
+    else
+        return ''
+    end
 end
 
 local function instance(_node_str)
