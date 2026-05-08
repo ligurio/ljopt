b71ed1fd894e2974bd0f06f811149d1c44fff4c7
Churkin Aleksey <lekhachurkin@gmail.com>
2026-05-08 17:55:49 +0300

    ljopt: do not pass SSA argument
    CNEW and SLOAD were passing the SSA reference as the first
    argument, which was incorrect. Drop it so they do not binded to
    any slot in VM stack.

--- ljopt/ir/SLOAD.lua
+++ ljopt/ir/SLOAD.lua
@@ -40,7 +40,7 @@ ir_node.extended(impls.IRNodeSLOADTab, ir_node.ir_node_base)
 function impls.IRNodeSLOADTab:to_smt_lib(ctx)
     local slot = ir_node.retrieve_slot_op(self:get_left_op())
     local ssa_ref = self:get_ssa_reference()
-    local mem_slot, smt_fm = ctx.mem_stack:allocate(ssa_ref, slot)
+    local mem_slot, smt_fm = ctx.mem_stack:allocate(slot)
     ctx.tab_info[ssa_ref] = {mem_ref = mem_slot}
     return ('%s\n%s\n%s'):format(
         -- I suppose guard for SLOAD is always true for us.
@@ -59,7 +59,7 @@ ir_node.extended(impls.IRNodeSLOADCdt, ir_node.ir_node_base)
 function impls.IRNodeSLOADCdt:to_smt_lib(ctx)
     local slot = ir_node.retrieve_slot_op(self:get_left_op())
     local ssa_ref = self:get_ssa_reference()
-    local mem_slot, smt_fm = ctx.mem_stack:allocate(ssa_ref, slot)
+    local mem_slot, smt_fm = ctx.mem_stack:allocate(slot)
     ctx.tab_info[ssa_ref] = {mem_ref = mem_slot}
     return ('%s\n%s'):format(
         -- I suppose guard for SLOAD is always true for us.
