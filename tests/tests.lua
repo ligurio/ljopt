9e8270b5f661958d48299e49c24d9303937cf5f9
Sergey Bronnikov <estetus@gmail.com>
2026-09-04 15:41:13 +0300

    ljopt: support u64.num CONV (num -> uint64)
    A CONV with destination u64 and source num is recorded by LuaJIT
    when a double is widened to a uint64_t (e.g. the traced loop counter
    passed to ffi.new("uint64_t", i)) on an unoptimized trace. CONV.lua
    only knew the i64.num and u32.num modes and aborted translation with
    "Unsupported type conversion: u64.num none".
    
    Encoding a double to a u64 follows C semantics: truncation toward
    zero (RTZ) of the unsigned 64-bit value, i.e. the 64-bit
    counterpart of the existing u32.num branch -
    `((_ fp.to_ubv 64) RTZ <num>)`.
    
    The patch adds the new mode next to i64.num and a regression test.
    
    Fixes #81

--- tests/tests.lua
+++ tests/tests.lua
@@ -1917,6 +1917,23 @@ end
             {type = "u64", name = "CONV",
                 right_op = op_type.new("lit", "u64.int sext")},
         },
+    }, {
+        name = "CONV u64.num",
+        code = [[
+local ffi = require('ffi')
+local function foo(n)
+    ffi.new('uint64_t', n)
+end
+
+foo(1)
+foo(2)
+foo(3)
+]],
+        opt = "jit.opt.start(3, 'hotloop=1', 'hotexit=1')",
+        ins = {
+            {type = "u64", name = "CONV",
+                right_op = op_type.new("lit", "u64.num none")},
+        },
     }}
     test:plan(2 * #srcs)
 
